87aebe08510ddd7c19820e832ebcc0def121c15d
Mehrdadshirvan <mehrdadshirvan@gmail.com>
2023-06-28 15:13:40 +0330

    template single page

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
-    path('<int:month>', views.monthly_challenges_by_number),
+    # path('<int:month>', views.monthly_challenges_by_number),
     path('<str:month>', views.monthly_challenges, name='month-challenge')
 ]
--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+# SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
