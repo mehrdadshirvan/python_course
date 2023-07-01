c70f02540911dc322378db3fe8d46335563d98e0
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2023-07-01 21:05:51 +0330

    generate header

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     # path('<int:month>', views.monthly_challenges_by_number),
     path('<str:month>', views.monthly_challenges, name='month-challenge')
 ]
--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates')
+            # BASE_DIR + "/" + templates
         ],
         'APP_DIRS': True,
         'OPTIONS': {
